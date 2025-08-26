b777fc1e53d7dd413019408d0ba3b4036b817dd4
Thamindu Disna <156172744+ThaminduDisnaZ@users.noreply.github.com>
2025-08-26 16:33:45 +0530

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { Alert, Button, Image, Pressable, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as ImagePicker from 'expo-image-picker';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [image, setImage] = useState<string | null>(null);
+    const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    
+    console.log(result);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+    return (
+      <ScrollView>
+        <View>
+          <Text>Create Account</Text>
+          <Text>Fill in the information below to create an account.</Text>
+        </View>
+        <View>
+          <Pressable onPress={pickImage}>
+            if (image != null) {
+              
+            }
+            
+          </Pressable>
+        </View>
+      </ScrollView>
+    );
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
