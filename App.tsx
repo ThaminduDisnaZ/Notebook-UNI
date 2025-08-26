98e5039061d38d1e902099a9c613683f0455d741
Thamindu Disna <156172744+ThaminduDisnaZ@users.noreply.github.com>
2025-08-26 17:43:42 +0530

    Refactor App component layout and styles for improved UI

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, Button, Image, Pressable, ScrollView, Text, View } from 'react-native';
+import { Alert, Button, Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as ImagePicker from 'expo-image-picker';
 export default function App() {
@@ -20,29 +20,84 @@ export default function App() {
     }
   }
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Create Account</Text>
-          <Text>Fill in the information below to create an account.</Text>
+
+      <ScrollView style={styles.container} contentContainerStyle={styles.scrollcontent}>
+        <SafeAreaView>
+        <View style={styles.header}>
+          <Text style={styles.pageTitle}>Create Account</Text>
+          <Text style={styles.subTitle}>Fill in the information below to create an account.</Text>
         </View>
         <View>
-          <Pressable onPress={pickImage}>
+          <Pressable onPress={pickImage} style={styles.imagePicker}>
             {image ? (
-              <Image source={{ uri: image }} />
+              
+                <Image source={{ uri: image }} style={styles.imageView}/>
+              
+             
             ) : (
-              <View>
+              <View style={styles.imagePlaceholder}>
                 <Text>+</Text>
                 <Text>Pick an image</Text>
               </View>
             )}
+            
           </Pressable>
+            <Text>{image}</Text>
         </View>
+          </SafeAreaView>
       </ScrollView>
-    </SafeAreaView >
+ 
   );
 
 }
 
 
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#b3ababff'
+  },
+  scrollcontent:{
+    flexGrow:1,
+    padding:20,
+    justifyContent:'space-between'
+  },
+  header:{
+    marginBottom:20,
+    alignItems:'center'
+  },
+  pageTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:10
+  },
+  subTitle:{
+    fontSize:16,
+    color:'#555'
+  },
+  imagePicker:{
+    width:180,
+    height:180,
+    borderRadius:90,
+    borderWidth:2,
+    borderStyle:'dashed',
+    backgroundColor:'#e1e1e1',
+    justifyContent:'center',
+    alignItems:'center',
+    alignSelf:'center',
+    overflow:'hidden'
+  },
+  imagePlaceholder:{
+    justifyContent:'center',
+    alignItems:'center',
+    fontSize:22,
+    color:'#555',
+    fontWeight:'bold'
+  },
+  imageView:{
+    width:'100%',
+    height:'100%',
+    resizeMode:'cover'
+  }
+});
